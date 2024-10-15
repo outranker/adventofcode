731145c4e23de58129c56dbfcb819c85967698db
outranker <outranker222@gmail.com>
2024-10-15 18:05:52 +0900

    update

--- neetcode/linked-lists/reverse-linked-list.ts
+++ neetcode/linked-lists/reverse-linked-list.ts
@@ -1,63 +1,85 @@
+import customDefaultsAssignIn from "https://deno.land/x/lodash@4.17.15-es/_customDefaultsAssignIn.js";
 import { assert } from "../../util/assert.mts";
 
-class Node {
-    value: number;
-    next: Node | null;
-    constructor(value: number, next = null) {
-        this.value = value;
+class ListNode {
+    val: number;
+    next: null | ListNode;
+    constructor(val = 0, next = null) {
+        this.val = val;
         this.next = next;
     }
 }
 
 class Solution {
-    static reverseList(head: Node): Node {
-        /**
-         * algo for reversing the linked list
-         * 1. get the last the node from old list
-         * 2. create new list and set it as the beginning node
-         * 3. remove that last node from the old list
-         * 4. again get the last node from the old list
-         * 5. set it as the last node for new list
-         * 6. remove the last node from the old list
-         * 7. rinse and repeat
-         * */
-
-        let newLinkedList: Node | null = null;
-
-        let lastNode: Node = head;
-        let ll: Node;
-        while (lastNode !== null) {
-            if (lastNode.next === null) ll = lastNode;
-            lastNode = lastNode.next as Node;
+    /**
+     * algo for reversing the linked list
+     * 1. get the last the node from old list
+     * 2. create new list and set it as the beginning node
+     * 3. remove that last node from the old list
+     * 4. again get the last node from the old list
+     * 5. set it as the last node for new list
+     * 6. remove the last node from the old list
+     * 7. rinse and repeat
+     * */
+    static reverseList(head: ListNode): ListNode {
+        if (!head) return head;
+        if (head.next === null) return head;
+        const arr = [];
+        let curr = head;
+        arr.push(head.val);
+        while (curr.next !== null) {
+            curr = curr.next;
+            arr.push(curr.val);
         }
 
-        // add to end of the list
-        if (!newLinkedList) {
-            newLinkedList = new Node(ll!.value);
-        } else {
-            while (newLinkedList !== null) {
-                newLinkedList = newLinkedList!.next;
-            }
-        }
-
-        return newLinkedList;
-    }
-
-    static fromArrayToList(args: number[]): Node {
-        let linkedList: Node | null = null;
-        for (const n of args) {
+        let linkedList: ListNode | null = null;
+        const l = arr.length;
+        for (let i = l - 1; i >= 0; i--) {
             if (!linkedList) {
-                linkedList = new Node(n);
+                linkedList = new ListNode(arr[i]);
             } else {
                 let t = linkedList;
                 while (t.next !== null) {
                     t = t.next;
                 }
-                t.next = new Node(n);
+                t.next = new ListNode(arr[i]);
             }
         }
-        return linkedList as Node;
+        return linkedList as ListNode;
+    }
+
+    static iterativeSolution(head: ListNode): ListNode {
+        let prev: ListNode | null = null;
+        let curr: ListNode = head;
+
+        while (curr?.next !== null) {
+            const temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        const t = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = t as unknown as ListNode;
+        return prev as unknown as ListNode;
+    }
+
+    static fromArrayToList(args: number[]): ListNode {
+        let linkedList: ListNode | null = null;
+        for (const n of args) {
+            if (!linkedList) {
+                linkedList = new ListNode(n);
+            } else {
+                let t = linkedList;
+                while (t.next !== null) {
+                    t = t.next;
+                }
+                t.next = new ListNode(n);
+            }
+        }
+        return linkedList as ListNode;
     }
 }
 
-console.log(Solution.reverseList(Solution.fromArrayToList([0, 1, 2, 3])));
+console.log(Solution.iterativeSolution(Solution.fromArrayToList([0, 1, 2, 3])));
